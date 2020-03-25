03203cea5add55ace5bb1c4d081acc4845956d33
fulviocanducci <fulviocanducci@hotmail.com>
2020-03-24 22:56:01 -0300

    First Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { useMyHook } from 'cep-hook'
+import { useCep } from 'cep-hook'
 
 const App = () => {
-  const example = useMyHook()
+  const [value, setValue, getZip] = useCep();
+  const [cep, setCep] = useState(null);
+  const handleClickFindCep = async() => {
+    try {
+      setCep(await getZip());
+    } catch (error) {
+      
+    }
+  }
   return (
     <div>
-      {example}
+      <div>
+        {cep && cep.localidade}
+      </div>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+      <button onClick={handleClickFindCep}>Busca de CEP</button>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,29 @@
 import * as React from 'react';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+export interface cep {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  unidade: string;
+  ibge: string;
+  gia: string;
+}
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
-
-  return counter;
-};
+export function useCep(): [string, React.Dispatch<React.SetStateAction<string>>, () => Promise<cep | undefined>] {
+  const [value, setValue] = React.useState<string>('');
+  const getZip = async(): Promise<cep | undefined> => {
+    try {
+      const response = await fetch(`http://viacep.com.br/ws/${value}/json/`, {mode: 'cors'});
+      if (response.ok){
+        return await response.json();
+      }
+    } catch (error) {
+      throw error
+    }
+    return undefined;
+  }
+  return [value, setValue, getZip];
+}
